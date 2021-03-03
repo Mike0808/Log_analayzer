da4880ac3da52466e43fa14ef4704b83ccebcd91
kuznetsov <kuznetsov@asd.ru>
2021-03-03 08:22:58 +0300

    Otus HW-1 Log analayzer Updated Main

--- main.py
+++ main.py
@@ -1,18 +1,22 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-
-
+import bz2
 import datetime
 import gzip
 import logging
+import fnmatch
+from statistics import median
+from collections import defaultdict, Counter
+from optparse import OptionParser
+import os
+import re
+from datetime import datetime
+
 # log_format ui_short '$remote_addr  $remote_user $http_x_real_ip [$time_local] "$request" '
 #                     '$status $body_bytes_sent "$http_referer" '
 #                     '"$http_user_agent" "$http_x_forwarded_for" "$http_X_REQUEST_ID" "$http_X_RB_USER" '
 #                     '$request_time';
-import os
-import re
-import sys
-from datetime import datetime
+
 
 config = {
     "REPORT_SIZE": 2,
@@ -22,21 +26,54 @@ config = {
 }
 
 
-# find latest log file
-def get_latest_ui_log(path):
-    file_dict = {}
-    for file in os.listdir(path):
-        if file.startswith("nginx-access-ui.log-"):
-            if not file.endswith('bz2'):
-                file_dict[re.split('\W', file)[4]] = file
-    file_dict = dict(sorted(file_dict.items(), reverse=True))
-    res = path + "/" + (list(file_dict.values())[0])
-    date = list(file_dict.keys())[0]
-    return res, date
+def file_find(filepat, top):
+    '''
+    Find all filenames in a directory tree that match a shell wildcard pattern
+    '''
+    pfile = set()
+    match = re.compile(r'\d+')
+    for path, _, filelist in os.walk(top):
+        break
+    for name in fnmatch.filter(filelist, filepat):
+        pfile.add(os.path.join(path, name))
+    fmax = max(pfile, key=lambda x: match.findall(x))
+    date = match.findall(fmax)
+    return fmax, date
+
+
+def gen_opener(filename):
+    '''
+    Open a sequence of filenames one at a time producing a file object.
+    The file is closed immediately when proceeding to the next iteration.
+    '''
+    if filename.endswith('.gz'):
+        f = gzip.open(filename, 'rt')
+    elif filename.endswith('.bz2'):
+        f = bz2.open(filename, 'rt')
+    else:
+        f = open(filename, 'rt')
+    yield f
+    f.close()
+
+
+# Finder for function above
+def find(pat, line):
+    '''
+    Find all matched pattern in line of text string
+    '''
+    match = pat.findall(line)
+    if match:
+        return match
+    else:
+        logging.error("Error with parsing log file")
+        return False
 
 
 # pattern finder in line of log file
 def get_requests_plain(line):
+    '''
+    Consume and produce all matched string of text
+    '''
     pat = (r''
            '^(\d+.\d+.\d+.\d+).+\['
            '(.+)\]\s'
@@ -49,127 +86,111 @@ def get_requests_plain(line):
            '"(.*?)"\s'
            '"(.*?)"\s'
            '(\d.\d+)')
-    requests = find(pat, line)
+    pat_cp = re.compile(pat)
+    requests = find(pat_cp, line)
     if requests:
-        res = list(*requests)
+        res = requests
         return res
-    return requests
-
-
-# Finder for function above
-def find(pat, line):
-    match = re.findall(pat, line)
-    if match:
-        return match
-    else:
-        logging.error("Error with parsing log file")
-        return False
+    return False
 
 
 # Log processor. Load log file
-def process_logs(file_name):
-    param_list = []
-    total = processed = 0
-    if file_name.endswith(".gz"):
-        with gzip.open(file_name, mode='rb') as log_file:
-            logging.info("Log gzip file opened for reading...")
-            for line in log_file:
-                line = str(line, 'utf-8')
-                parsed_line = get_requests_plain(line)
-                print(parsed_line)
-                total += 1
-                if parsed_line:
-                    processed += 1
-                    yield parsed_line, processed, total
-    else:
-        with open(file_name, encoding='utf-8', mode='r') as log_file:
-            logging.info("Log pure file opened for reading...")
-            for line in log_file:
-                parsed_line = get_requests_plain(line)
-                total += 1
-                if parsed_line:
-                    processed += 1
-                    yield parsed_line, processed, total
+def gen_process_logs(gen_file_name):
+    '''
+    Starter processing on log file.
+    Starter pattern find func and count some values of text,
+    like total and processed lines in a text and counter same url.
+    '''
+    processed = 0
+    api_counter = Counter()
+    for f in gen_file_name:
+        for total, line in enumerate(f, 1):
+            parsed_line = get_requests_plain(line)
+            if parsed_line:
+                processed += 1
+                api_counter[parsed_line[0][2]] += 1
+                pr = parsed_line[0]
+                yield [pr, processed, total, api_counter[pr[2]]]
 
 
 # sort addon
 def keyfunc(tup):
-    key, d = tup
-    return d["time_sum"]
+    '''
+    Func add-on for sorting dict method sorted().
+    '''
+    key, value = tup
+    return value["Time_sum"]
 
 
-def param_former(file_name, fail_size):
-    gr = process_logs(file_name)
-    param_list = [*gr]
-    distinct_set = {x[0][2]: [x[0], x[1], x[2]] for x in param_list}
-    distinct_list = list(distinct_set.keys())
-    distinct_length = len(distinct_set.keys())
-    lp = list(distinct_set.values())
-    total_lines = lp[-1][-1]
-    processed_lines = lp[-1][-2]
-    fault_total = total_lines - processed_lines
-    perc_fault = (fault_total * 100) / total_lines
-    if perc_fault > fail_size:
-        logging.error("Exceeded bound of log parsing fail... Exit")
-        exit()
-    return \
-        param_list, \
-        distinct_list, \
-        distinct_length, \
-        total_lines, \
-        processed_lines
-
-
-# Big function for prepare report file
-def compute_log(param_list, distinct_list, distinct_length, total_lines):
-    url = 2
-    request_time = -1
-    count = sum_request_time = max_time = all_sum_request_time = tmp_max_time = time_med = 0.0
+def gen_param_former(file_name, fail_size):
+    '''
+    Calc func for some parameters of report and former a dict with certain keys.
+    '''
     d = {}
-    temp_time_list = []
-    logging.info("Starting compute logs data...", exc_info=True)
-    for idi in range(distinct_length):
-        all_sum_request_time += float(param_list[idi][0][request_time])
-        for idj in range(total_lines):
-            if distinct_list[idi] in param_list[idj][0]:
-                count += 1
-                sum_request_time += float(param_list[idj][0][request_time])
-                if max_time < float(param_list[idj][0][request_time]):
-                    max_time = float(param_list[idj][0][request_time])
-                temp_time_list.append(str(param_list[idj][0][request_time]))
-        if count % 2:
-            time_med = round(float(temp_time_list[round(count / 2)]), 3)
-        else:
-            time_med = round((float(temp_time_list[round(count / 2)]) +
-                              float(temp_time_list[round(count / 2) - 1])) / 2, 3)
-        d[distinct_list[idi]] = {'url': param_list[idi][0][url], 'count_perc': (count * 100) / total_lines,
-                                 'count': count, 'time_sum': sum_request_time,
-                                 'time_max': max_time, 'time_perc': (max_time * 100) / all_sum_request_time,
-                                 'time_avg': sum_request_time / count, 'time_med': time_med}
-        sum_request_time = 0.0
-        max_time = 0.0
-        time_med = 0.0
-        count = 0
-        temp_time_list.clear()
-    return d
+    time_req = defaultdict(list)
+    gr = gen_process_logs(file_name)
+    sum_req_time = 0.0
+    for it in gr:
+        api = it[0][2]
+        proc_count = it[1]
+        total_line = it[2]
+        api_count = it[3]
+        sum_req_time += float(it[0][-1])
+        time_req[api].append(float(it[0][-1]))
+        time_sum = sum(time_req[api])
+        time_max = max(time_req[api])
+        time_avg = (time_sum/len(time_req[api]))
+        time_med = median(time_req[api])
+        fault_total = total_line - proc_count
+        perc_fault = (fault_total * 100) / total_line
+        if perc_fault > fail_size:
+            logging.error("Exceeded bound of log parsing fail... Exit")
+            exit()
+        d[api] = {'Url': api,
+                  'Count': api_count,
+                  'Time_sum': time_sum,
+                  'Time_max': time_max,
+                  'Time_perc': 0,
+                  'Count_perc': 0,
+                  'Time_med': time_med,
+                  'Time_avg': time_avg}
+        yield d, sum_req_time, total_line
 
 
 def sort_dict(unsorted_dict, report_size_count):
-    d_out = []
-    report_size_count = int(report_size_count)
-    d_sorted = sorted(unsorted_dict.items(), key=keyfunc, reverse=True)
-    if report_size_count > len(d_sorted):
-        report_size_count = len(d_sorted)
-        print("Line count of log less then your request... Taked max count is {}".format(report_size_count))
+    '''
+    Endind calculation and sorting dict from previous step.
+    :param unsorted_dict:
+    :param report_size_count:
+    :return:
+    '''
+    total_lines = 0
+    sum_req_time = 0.0
+    for items in unsorted_dict:
+        out = items[0]
+        total_lines = items[2]
+        sum_req_time = items[1]
+    for k, v in out.items():
+        v['Time_perc'] = round((v['Time_sum'] * 100) / sum_req_time, 3)
+        v['Count_perc'] = round((v['Count'] * 100) / total_lines, 3)
+    d_outer = sorted(out.items(), key=keyfunc, reverse=True)
+    if len(d_outer) < report_size_count:
         logging.error("Line count of log less then your request...", exc_info=True)
-    for idx in range(report_size_count):
-        d_out.append(d_sorted[idx][1])
+        return False
+    d_outer = d_outer[:report_size_count]
     logging.info("Compute and sort log finished...")
-    return d_out
+    return d_outer
 
 
 # create prepared report
 def create_output_report(path, date, input_data):
+    '''
+    Creating output report with defined struct.
+    :param path:
+    :param date:
+    :param input_data:
+    :return:
+    '''
     logging.info("Creating output log...")
     date = datetime.strptime(date, "%Y%m%d")
     date = str(date).replace("-", ".").split(" ")[0]
@@ -186,84 +207,30 @@ def create_output_report(path, date, input_data):
         print("The latest log file is computed and report made.")
 
 
-# Config file parser
-def get_config(path):
-    config_dict = {}
-    flag_config = False
-    with open(path, encoding='utf-8', mode='r') as template:
-        for line in template:
-            if line:
-                if line.startswith("[config]"):
-                    flag_config = True
-                    continue
-                if flag_config:
-                    if line.startswith("report_size"):
-                        config_dict[line.split("=")[0]] = line.split("=")[1].strip()
-                    elif line.startswith("report_dir"):
-                        config_dict[line.split("=")[0]] = line.split("=")[1].strip()
-                    elif line.startswith("log_dir"):
-                        config_dict[line.split("=")[0]] = line.split("=")[1].strip()
-                    elif line.startswith("fail_size"):
-                        config_dict[line.split("=")[0]] = line.split("=")[1].strip()
-                else:
-                    return False
-        return config_dict
-
-
-# Choiser parameters
-def arg_switcher(args_list):
-    switcher = {
-        1: "--report_size",
-        2: "--report_dir",
-        3: "--log_dir",
-        4: "--fail_size",
-        5: "--config"
-    }
-    d = {}
-    report_dir = config["REPORT_DIR"]
-    log_dir = config["LOG_DIR"]
-    report_size = config["REPORT_SIZE"]
-    fail_size = config["FAIL_SIZE"]
-    nargs = len(args_list)
-    for idx in range(nargs):
-        arg = args_list[idx]
-        if arg in switcher.values():
-            d[arg.split("--")[1]] = args_list[idx + 1]
-    if "config" in d.keys():
-        config_file = d["config"]
-        d.update(get_config(config_file))
-    if "report_dir" in d.keys():
-        report_dir = d["report_dir"]
-    if "log_dir" in d.keys():
-        log_dir = d["log_dir"]
-    if "report_size" in d.keys():
-        report_size = d["report_size"]
-    if "fail_size" in d.keys():
-        fail_size = d["fail_size"]
-    return report_dir, log_dir, report_size, fail_size
-
-
 def main():
-    d = {}
-    report_dir = None
-    log_dir = None
-    report_size_count = None
-    fail_size = None
-    nargs = len(sys.argv)
-    usage = 'usage: {} --log_dir --report_dir --report_size, --fail_size, --config'.format(os.path.basename(sys.argv[0]))
-    logging.basicConfig(filename='app.log',
+    op = OptionParser()
+    op.add_option("-r", "--reportsize", action="store", type=str, help="Max report size",
+                  default=config['REPORT_SIZE'])
+    op.add_option("-d", "--reportdir", action="store", type=str, help="Where will be saved processed reports",
+                  default=config['REPORT_DIR'])
+    op.add_option("-g", "--logdir", action="store", type=str, help="From where will be processed logs",
+                  default=config['LOG_DIR'])
+    op.add_option("-f", "--failsize", action="store", type=str, help="Max allowable fail in processing",
+                  default=config['FAIL_SIZE'])
+    op.add_option("-l", "--log", action="store", type=str, help="Log filename.", default="app.log")
+    (opts, args) = op.parse_args()
+    logging.basicConfig(filename=opts.log,
                         filemode='w',
                         format='[%(asctime)s] %(levelname).1s %(message)s',
                         datefmt='%Y.%m.%d %H:%M:%S')
-    print(usage)
     try:
-        report_dir, log_dir, report_size, fail_size = arg_switcher(sys.argv)
-        file_name, date = get_latest_ui_log(log_dir)
-        param_list, distinct_list, distinct_length, total_lines, processed_lines = \
-            param_former(file_name, fail_size)
-        computed_log = compute_log(param_list, distinct_list, distinct_length, total_lines)
-        sorted_dict = sort_dict(computed_log, report_size)
-        create_output_report(report_dir, date, sorted_dict)
+        ffile = file_find("nginx-access-ui.log-*", opts.logdir)
+        filename = ffile[0]
+        date = ffile[1][0]
+        fopen = gen_opener(filename)
+        gendict = gen_param_former(fopen, opts.failsize)
+        sorted_dict = sort_dict(gendict, opts.reportsize)
+        create_output_report(opts.reportdir, date, sorted_dict)
     except Exception as e:
         logging.exception("Exception occurred", exc_info=True)
 
--- test.py
+++ test.py
@@ -7,65 +7,46 @@ import os
 
 class LogTest(unittest.TestCase):
 
-    def test_get_latest_ui_log(self):
-        self.assertEqual(main.get_latest_ui_log("./test_dir"), ('./test_dir/nginx-access-ui.log-20190630.gz', '20190630'))
+    def test_file_find(self):
+        self.assertEqual(main.file_find("nginx-access-ui.log-*", "./test_dir"), ('./test_dir/nginx-access-ui.log-20190630.gz', ['20190630']))
 
     def test_get_requests_plain(self):
         with open("./test_dir/nginx-access-ui.log-20190629", mode="r") as testfile:
             line = testfile.readline()
             self.assertIsInstance(main.get_requests_plain(line), list)
-
-    def test_find(self):
-        with open("./test_dir/nginx-access-ui.log-20190629", mode="r") as testfile:
-            line = testfile.readline()
-            self.assertEqual(main.find("^(\d+.\d+.\d+.\d+).+\[", line), ["1.196.116.32"])
+            self.assertEqual(main.get_requests_plain(line), [('1.196.116.32', '29/Jun/2017:03:50:22 +0300', '/api/v2/banner/25019354', 'HTTP/1.1', '" 200 ', '927 ', '-', 'Lynx/2.8.8dev.9 libwww-FM/2.14 SSL-MM/1.4.1 GNUTLS/2.10.5', '-', '1498697422-2190034393-4708-9752759" "dc7161be3', '0.390')])
 
     def test_process_logs(self):
-        self.assertIsInstance(main.process_logs("./test_dir/nginx-access-ui.log-20190629"), types.GeneratorType)
-        tr = main.process_logs("./test_dir/nginx-access-ui.log-20190629")
+        ffile = main.file_find("nginx-access-ui.log-*", "./test_dir")
+        filename = ffile[0]
+        date = ffile[1][0]
+        fopen = main.gen_opener(filename)
+        self.assertIsInstance(main.gen_process_logs(fopen), types.GeneratorType)
+        tr = main.gen_process_logs(fopen)
         tv = [x[1] for x in tr]
         for idx in range(len(tv)):
             self.assertEqual(tv[idx], idx+1)
 
-    def test_compute_log(self):
-        self.assertIsInstance(main.compute_log("./test_dir/nginx-access-ui.log-20190629", 10, 10), list)
-        len_list = len(main.compute_log("./test_dir/nginx-access-ui.log-20190629", 10, 10))
-        self.assertEqual(len_list, 10)
+    def test_gen_param_former(self):
+        ffile = main.file_find("nginx-access-ui.log-*", "./test_dir")
+        filename = ffile[0]
+        date = ffile[1][0]
+        fopen = main.gen_opener(filename)
+        self.assertIsInstance(main.gen_param_former(filename, 3), types.GeneratorType)
+        gen = main.gen_param_former(fopen, 3)
+        tv = [x for x in gen]
+        self.assertIsInstance(tv, list)
+        self.assertEqual(len(tv[0]), 3)
+        self.assertIsInstance(tv[1][0], dict)
+        self.assertEqual(tv[1][0].get('/api/v2/banner/25019354')['Url'], '/api/v2/banner/25019354')
+        self.assertIsInstance(tv[1][1], float)
+        self.assertIsInstance(tv[1][2], int)
 
     def test_create_output_report(self):
         path = "./test"
         self.assertEqual(os.path.exists(path), True)
         date = "20170723"
         file_name = path + '/report-' + "2017.07.23" + '.html'
-        self.assertEqual(os.path.isfile(file_name), True)
+        self.assertEqual(os.path.isfile(file_name), False)
         main.create_output_report(path, date, "sdasdasd")
         self.assertEqual(os.path.isfile(file_name), True)
-
-    def test_get_config(self):
-        test_dict = main.get_config("./test_dir/config.conf")
-        self.assertIsInstance(test_dict, dict)
-        self.assertEqual(len(test_dict), 3)
-        self.assertEqual(int(test_dict["report_size"]), 100)
-
-    def test_arg_switcher(self):
-        switcher = [
-            "--report_size",
-            200,
-            "--report_dir",
-            "./test_dir",
-            "--log_dir",
-            "./test_dir",
-            "--fail_size",
-            10,
-            "--config",
-            "./test_dir/config.conf"
-        ]
-        test_dict = {
-            "--report_size": 100,
-            "--report_dir": "./test_dir",
-            "--log_dir": "./test_dir",
-            "--fail_size": 10,
-            "--config": "./test_dir/config.conf"
-        }
-        self.assertEqual(main.arg_switcher(switcher), ('./test_dir', './test_dir', '100', '10'))
-
